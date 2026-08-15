ee01c44e666f359c55bce6d500ef1954d87b9571
ibrahimyahia1 <ibrahimyahia091@gmail.com>
2026-08-15 21:19:07 +0300

    update app module to import MCP module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { McpModule } from './mcp/mcp.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, McpModule],
   controllers: [AppController],
   providers: [AppService],
 })
